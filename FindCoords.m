67d8bd931ca2f419416d29f32dfe912aa2c2da2f
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-08 16:03:32 +0100

    Created the importer function

---
+++ FindCoords.m
@@ -0,0 +1,8 @@
+close, clear, clc
+
+jpeg_cell_au = importer('C:\Users\jg17acv\Documents\DevelopmentEngineer\UH-PPD-LC\PPD2\SID3-PPD2\NetworkTest\[150091716]');
+
+
+
+
+
--- importer.m
+++ importer.m
@@ -1,8 +1,22 @@
-function [ jpeg_struct ] = importer( data_dir )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [ jpeg_cell ] = importer( data_dir )
+%IMPORTER imports jpegs in a folder to a cell for analysis
+%   Each cell element of 'jpeg_cell' is intensity in arbitrary units, the
+%   index is pixel number (X,Y). 'data_dir' is the directory containing the
+%   jpegs to be analysed.
 
+%%  Find all the files with .jpg extension in the data directory
+search_string = char([data_dir, '\*jpg']);
+list = dir(search_string);
+[files, ~] = size(list);    % Get number of fiels for loop
+jpegs = cell(files, 1);     % Pre-allocate jpeg cell for speediness!
 
+%%  Cycle thoguh files and extract data using imread
+for i=1:files
+    path = char([list(i).folder, '\', list(i).name]);
+    intensity_au = imread(path);    % Read file
+    jpegs{i, 1} = intensity_au;     % Assign file to array
+end
+jpeg_cell = jpegs;                  % Assign output from function
 
 end
 
