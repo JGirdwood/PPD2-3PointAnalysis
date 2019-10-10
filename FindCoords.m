60264331e2166ab56be84618eb1818d5c125270c
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-10 21:54:53 +0100

    swapped x and y offsets

--- FindCoords.m
+++ FindCoords.m
@@ -1,7 +1,7 @@
 close, clear, clc
 
 jpeg_cell_au = Importer('C:\Users\jg17acv\Documents\DevelopmentEngineer\UH-PPD-LC\PPD2\SID3-PPD2\NetworkTest\[150091716]');
-[px, py] = size(jpeg_cell_au{1});
+[py, px] = size(jpeg_cell_au{1});
 offset_xy = [round(px/2), round(py/2)];
 [ E1_xy, E2_xy, E3_xy ] = r2xy( 200, offset_xy );
 [r, ~] = size(jpeg_cell_au);
