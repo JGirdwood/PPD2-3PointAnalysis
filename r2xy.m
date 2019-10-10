87e7f8d31ccff475565a2cb4731b7b2b6d9af5d2
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-10 15:37:42 +0100

    Create r2xy.m

---
+++ r2xy.m
@@ -0,0 +1,15 @@
+function [ E1_xy, E2_xy, E3_xy ] = r2xy( rad, angle_offset )
+%RA2XY Transforms a radius and angle offset on a PPD2 image to xy 
+%coordinates
+%   E*_xy is the outlut coordinates of the photodoides in pixels, r is the
+%   radius in pixels, and angle offset is the offset in the angle of the
+%   first doide (given equal spacing).
+
+if nargin<2
+  angle_offset = 0;
+end
+
+
+
+end
+
