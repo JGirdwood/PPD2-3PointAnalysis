9fb25b6e0f6e36968a9ed44512da0091df856e97
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-11 16:18:33 +0100

    Updated FindCoords to work with E123 and AF123

--- FindCoords.m
+++ FindCoords.m
@@ -1,6 +1,6 @@
 %close, clear, clc
 
-%jpeg_cell_au = Importer('C:\Users\jg17acv\Documents\DevelopmentEngineer\UH-PPD-LC\PPD2\SID3-PPD2\NetworkTest\[150091716]');
+jpeg_cell_au = Importer('C:\Users\jg17acv\Documents\DevelopmentEngineer\UH-PPD-LC\PPD2\SID3-PPD2\NetworkTest\[150091716]');
 
 [py, px] = size(jpeg_cell_au{1});
 offset_xy = [round(px/2), round(py/2)];
@@ -11,12 +11,21 @@ rad_ulim = round((py-25)/2);
 rad_llim = 50;
 
 AF_store = zeros(r, rad_ulim - rad_llim);
+AR123_store = zeros(r, rad_ulim - rad_llim);
+
 AF_index = 1;
-for j=1:rad_llim-rad_ulim
+for j=1:rad_ulim-rad_llim
+    
     [ E1_xy, E2_xy, E3_xy ] = r2xy( j, offset_xy );
+    
     for i=1:r
-        AF_store(i, AF_index) = AsymetryFactor ...
+        
+        [AF_store(i, AF_index), E123] = AsymetryFactor ...
             ( E1_xy, E2_xy, E3_xy, PMT_size, jpeg_cell_au{i} );
+        AR123_store(i, AF_index) = E123toPolar( E123 );
+        
     end
+    
     AF_index = AF_index + 1;
+    
 end
--- TriangleScatter.m
+++ TriangleScatter.m
@@ -1,9 +1,9 @@
-function [ scatter_fig ] = TriangleScatter( polar_AF_array )
+function [ scatter_fig ] = TriangleScatter( AR123_array )
 %TRIANGLESCATTER Makes a scatter plot from AF data
 %   outputs the figure object and uses AF data from multiple images of the 
 %   same catagory (e.g. droplets).
 
-[r, c] = size(polar_AF_array);
+[r, c] = size(AR123_array);
 fig = figure();
 
 end
