ca259d3d2b2167d0fc75bc1ff04d96256015e432
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-11 15:22:07 +0100

    Added plotting function

--- FindCoords.m
+++ FindCoords.m
@@ -1,18 +1,20 @@
 %close, clear, clc
 
 %jpeg_cell_au = Importer('C:\Users\jg17acv\Documents\DevelopmentEngineer\UH-PPD-LC\PPD2\SID3-PPD2\NetworkTest\[150091716]');
+%jpeg_cell_au = Importer('C:\Users\jg17acv\Documents\DevelopmentEngineer\UH-PPD-LC\PPD2-sorted\Droplets');
+jpeg_cell_au = Importer('C:\Users\jg17acv\Documents\DevelopmentEngineer\UH-PPD-LC\PPD2-sorted\Unclassified-Solid');
 
 [py, px] = size(jpeg_cell_au{1});
 offset_xy = [round(px/2), round(py/2)];
 [r, ~] = size(jpeg_cell_au);
-PMT_size = 20;
+PMT_size = 50;
 
 rad_ulim = round((py-25)/2);
 rad_llim = 50;
 
 AF_store = zeros(r, rad_ulim - rad_llim);
 AF_index = 1;
-for j=1:rad_llim-rad_ulim
+for j=1:rad_ulim-rad_llim
     [ E1_xy, E2_xy, E3_xy ] = r2xy( j, offset_xy );
     for i=1:r
         AF_store(i, AF_index) = AsymetryFactor ...
