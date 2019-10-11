d2d4a5542b985e5a5429061eb6a0685d9440320e
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-11 15:43:27 +0100

    Create E123toPolar.m

---
+++ E123toPolar.m
@@ -0,0 +1,9 @@
+function [ RA123 ] = E123toPolar( E123 )
+%E123TOPOLAR Convert the 3 detector energies to a polar vetcor centroid.
+%   RA123 is the radius and angle of the point and E123 is the 3 detector
+%   energies in the following format [E1, E2, E3].
+
+
+
+end
+
