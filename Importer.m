e8c1d0bee13a72b3734188501472a4e3133f7758
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-11 15:31:03 +0100

    Create Importer.m

---
+++ Importer.m
@@ -0,0 +1,21 @@
+function [ jpeg_cell ] = Importer( data_dir )
+%IMPORTER imports jpegs in a folder to a cell for analysis
+%   Each cell element of 'jpeg_cell' is intensity in arbitrary units, the
+%   index is pixel number (X,Y). 'data_dir' is the directory containing the
+%   jpegs to be analysed.
+
+%%  Find all the files with .jpg extension in the data directory
+search_string = char([data_dir, '\*jpg']);
+list = dir(search_string);
+[files, ~] = size(list);    % Get number of fiels for loop
+jpegs = cell(files, 1);     % Pre-allocate jpeg cell for speediness!
+
+%%  Cycle thoguh files and extract data using imread
+for i=1:files
+    path = char([list(i).folder, '\', list(i).name]);
+    intensity_au = imread(path);    % Read file
+    jpegs{i, 1} = intensity_au;     % Assign file to array
+end
+jpeg_cell = jpegs;                  % Assign output from function
+
+end
