aef7b151a5da9488ba71312ab764502bee9c49d2
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-14 17:18:28 +0100

    Added baseline for main loop

--- FindCoords.m
+++ FindCoords.m
@@ -24,8 +24,13 @@ radius_step = 50;           % Step in radius for loopiness
 % Parameters for the image loop
 particle_types = 2;         % Number of different particle types
 
+% Parameters for the class error loop
+PMT_size_min = 10;          % Min PMT size in pixels
+PMT_size_max = 150;         % Max PMT size in pixels
+PMT_size_step = 10;         % Step in PMT size loop in pixels
+
 % Parameters for the camera area
-PMT_size = 70;              % Detector size in pixels
+PMT_size = 70;              % Detector size in pixels (default)
 image_radius = 279;         % Radius of image in pixels
 beamstop_radius = 50;       % Radius of centre beamstop in pixels
 default_radius = 150;       % Radius for the image loop
@@ -140,13 +145,36 @@ elseif loop == 1
     class_store{1, 1} = AF_store(:, 1) > AF_droplet;
     class_store{1, 2} = AF_store(:, 2) < AF_solid;
     
-    percent_error_droplet = sum(class_store{1, 1})/rd*100
-    percent_error_solid = sum(class_store{1, 2})/rd*100
+    percent_error_droplet = sum(class_store{1, 1})/rd*100;
+    percent_error_solid = sum(class_store{1, 2})/rd*100;
+
+% 3) Classification error loop and plotting
+elseif loop == 2
+    
+    % Get radius upper and low limits (and specify step)
+    rad_ulim = image_radius;
+    rad_step = radius_step;
+    rad_llim = beamstop_radius;
+    
+    % Pre-allocate storage variables for asumetry factor (AF) and centroid
+    % angle radius for the 3 combined detectors (AR123)
+    AF_store = zeros(rd, ceil((rad_ulim - rad_llim)/rad_step));
+    AR123_store = cell(rd, ceil((rad_ulim - rad_llim)/rad_step));
+
+    AF_index_rad = 1;       % Index for storage variables in loop
+    
+    for j=1:rad_step:rad_ulim-rad_llim
+        
+        % ~~~~~
+        
+        AF_index_rad = AF_index_rad + 1;
+        
+    end
     
 end
 
 if error == true
-    "Errors were encountered"
+    "Errors were encountered";
 end
 
 
