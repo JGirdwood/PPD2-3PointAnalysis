dfa66a83564eb3f816b797c63fd2c9823f509856
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2019-10-18 11:17:55 +0100

    Added a loop to the conversion

--- Pix2AD.m
+++ Pix2AD.m
@@ -3,18 +3,24 @@ function [ half_angle, rad_mm ] = Pix2AD( rad_pix )
 %   Converts radial distance in pixels to radial distance in mm and
 %   scattering angle.
 
-PPD2_max_pix = 780/2;           % Number of pixels at the max half angle
-LUT = load("ppd2angles.mat");   % Lookup table for angle and mm
-LUT = LUT.ppd2angles;
-[r, ~] = size(LUT);
-pixel_arr = 0:PPD2_max_pix/r:PPD2_max_pix;
-pixel_arr = pixel_arr';
-LUT = [[[0,0];LUT], pixel_arr];
-HA_interp = griddedInterpolant(LUT(:,end), LUT(:,1));
-rad_interp = griddedInterpolant(LUT(:,end), LUT(:,2));
+[~, c] = size(rad_pix);
+half_angle = zeros(1, c);
+rad_mm = zeros(1, c);
 
-half_angle = HA_interp(rad_pix);
-rad_mm = rad_interp(rad_pix);
+for i=1:c
+    PPD2_max_pix = 780/2;           % Number of pixels at the max angle
+    LUT = load("ppd2angles.mat");   % Lookup table for angle and mm
+    LUT = LUT.ppd2angles;
+    [r, ~] = size(LUT);
+    pixel_arr = 0:PPD2_max_pix/r:PPD2_max_pix;
+    pixel_arr = pixel_arr';
+    LUT = [[[0,0];LUT], pixel_arr];
+    HA_interp = griddedInterpolant(LUT(:,end), LUT(:,1));
+    rad_interp = griddedInterpolant(LUT(:,end), LUT(:,2));
+
+    half_angle(1, i) = HA_interp(rad_pix);
+    rad_mm(1, i) = rad_interp(rad_pix);
+end
 
 end
 
